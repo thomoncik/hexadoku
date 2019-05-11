08f7f4c4278b7ba534c45e906576d37f01ae5fe1
kkiermasz <kiermasz.k@gmail.com>
2019-05-11 13:15:13 +0200

    Create board section.

---
+++ include/Views/BoardSection.hpp
@@ -0,0 +1,25 @@
+//
+// Created by Jakub Kiermasz on 2019-05-11.
+//
+
+#ifndef HEXADOKU_BOARDSECTION_HPP
+#define HEXADOKU_BOARDSECTION_HPP
+
+#include <vector>
+#include "BoardCell.hpp"
+
+class BoardSection {
+public:
+    BoardSection(int size);
+    void SetSelected(bool isSelectd, int row, int column);
+    void SetValue(int value, int row, int column);
+    void SetIsCorrect(bool isCorrect, int row, int column);
+    std::vector<int> ValuesInRow(int row);
+    std::vector<int> ValuesInColumn(int column);
+private:
+    std::vector<BoardCell> cells;
+    int size;
+};
+
+
+#endif //HEXADOKU_BOARDSECTION_HPP
---
+++ src/Views/BoardSection.cpp
@@ -0,0 +1,34 @@
+//
+// Created by Jakub Kiermasz on 2019-05-11.
+//
+
+#include "../../include/Views/BoardSection.hpp"
+BoardSection::BoardSection(int size) : size(size), cells(std::vector<BoardCell>(9)) {}
+
+void BoardSection::SetSelected(bool isSelectd, int row, int column) {
+    cells[row * size + column].SetSelected(isSelectd);
+}
+
+void BoardSection::SetIsCorrect(bool isCorrect, int row, int column) {
+    cells[row * size + column].SetIsCorrect(isCorrect);
+}
+
+void BoardSection::SetValue(int value, int row, int column) {
+    cells[row * size + column].SetValue(value);
+}
+
+std::vector<int> BoardSection::ValuesInColumn(int column) {
+    std::vector<int> values(size);
+    for (int i = 0 + column; i < size; i += size) {
+        values.push_back(cells[i].GetValue());
+    }
+    return values;
+}
+
+std::vector<int> BoardSection::ValuesInRow(int row) {
+    std::vector<int> values(size);
+    for (int i = 0 + row * size; i < size; i += size) {
+        values.push_back(cells[i].GetValue());
+    }
+    return values;
+}
