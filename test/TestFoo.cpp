07821b472e114a2422eb43e5bdf785a89bb53140
Tomasz Homoncik <tjhomoncik@gmail.com>
2019-05-11 18:15:23 +0200

    Remove unnecessary test file

--- test/TestFoo.cpp
+++
@@ -1,7 +0,0 @@
-#include "../vendor/include/catch2/catch.hpp"
-#include "../vendor/include/fakeit/fakeit.hpp"
-
-TEST_CASE( "True is true", "[always]" ) {
-REQUIRE( true );
-//REQUIRE( false != true );
-}
