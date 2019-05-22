8b3ab105d79d77e21a06f735366ce2a4638a74ce
kkiermasz <kiermasz.k@gmail.com>
2019-05-22 11:24:23 +0200

    Create insertion mode.

---
+++ include/MovePlayState.hpp
@@ -0,0 +1,19 @@
+//
+// Created by thomoncik on 09.05.19.
+//
+
+#ifndef HEXADOKU_MOVEPLAYSTATE_HPP
+#define HEXADOKU_MOVEPLAYSTATE_HPP
+
+#include "Model/Board.hpp"
+#include "PlayStateAbstract.hpp"
+
+
+class MovePlayState : public PlayStateAbstract {
+public:
+    explicit MovePlayState(int boardSize, std::shared_ptr<Board> board = nullptr, int x = 0, int y = 0);
+    void HandleInput(Game &game, char input) override;
+};
+
+
+#endif //HEXADOKU_MOVEPLAYSTATE_HPP
---
+++ src/InsertionPlayState.cpp
@@ -0,0 +1,27 @@
+//
+// Created by Jakub Kiermasz on 2019-05-25.
+//
+
+#include "InsertionPlayState.hpp"
+#include <MenuState.hpp>
+#include "MovePlayState.hpp"
+#include <algorithm>
+#include <cstddef>
+
+InsertionPlayState::InsertionPlayState(int boardSize, std::shared_ptr<Board> board, int x, int y) : PlayStateAbstract(boardSize, board, x, y), boardCharacters(boardSize == Board::STANDARD_SIZE ?
+                            std::vector<char>{' ', '1', '2', '3', '4', '5', '6', '7', '8', '9'} :
+                            std::vector<char>{' ', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e'}) {}
+
+void InsertionPlayState::HandleInput(Game &game, char input) {
+    board->SetSelected(false, x, y);
+
+    if (input == 'q') {
+        game.SetState(MenuState::MAIN_MENU);
+    } else if (input == 'm') {
+        game.SetState(new MovePlayState(this->board->GetSize(), this->board, this->x, this->y));
+    } else if (std::find(boardCharacters.begin(), boardCharacters.end(), input) != boardCharacters.end()) {
+        ptrdiff_t value = distance(boardCharacters.begin(), find(boardCharacters.begin(), boardCharacters.end(), input));
+        this->board->SetValue(value, x, y);
+    }
+    this->board->SetSelected(true, x, y);
+}
